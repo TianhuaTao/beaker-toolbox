c80368846bf8265107797c13d7c7f6a966393b84
Tianhua Tao <taotianhua@outlook.com>
2025-07-11 07:22:13 +0000

    a

--- tmp.sh
+++ tmp.sh
@@ -1,5 +1,6 @@
 bash /workspace/beaker-toolbox/run_script_remotely.sh /workspace/hostfile /workspace/beaker-toolbox/node_cmd-ablation-train-general.sh OLMoE3-dev-32l-jul10-gcp
 bash /workspace/beaker-toolbox/run_script_remotely.sh /workspace/hostfile /workspace/beaker-toolbox/node_cmd-ablation-train-general.sh OLMoE3-dev-32l-jul10-gcp-s1
+bash /workspace/beaker-toolbox/run_script_remotely.sh /workspace/hostfile /workspace/beaker-toolbox/node_cmd-ablation-train-general.sh OLMoE3-dev-48l-jul10-gcp-s1
 
 
 
