a9d42a23cb404306c1a5aa5b2437df130508319a
Tianhua Tao <taotianhua@outlook.com>
2025-07-12 18:31:14 -0700

    u

--- tmp.sh
+++ tmp.sh
@@ -3,7 +3,7 @@ bash /workspace/beaker-toolbox/run_script_remotely.sh /workspace/hostfile /works
 bash /workspace/beaker-toolbox/run_script_remotely.sh /workspace/hostfile /workspace/beaker-toolbox/node_cmd-ablation-train-general.sh OLMoE3-dev-48l-jul10-gcp-s1
 
 
-"kill -9 $(ps aux | grep -e "train/OLMoE3-" -e "nsys" | grep -v grep | awk '{print $2}')"
+kill -9 $(ps aux | grep -e "train/OLMoE3-" -e "nsys" | grep -v grep | awk '{print $2}')
 
 while read -r host; do
     ssh -p30255 -o "StrictHostKeyChecking no" "$host" "kill -9 \$(ps aux | grep -e "train/OLMoE3-" -e "nsys" | grep -v grep | awk '{print \$2}')" &
