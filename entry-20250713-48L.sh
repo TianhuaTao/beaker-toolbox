16d8e8298a338638e4c40a7fb60a2a11b11ab976
Tianhua Tao <taotianhua@outlook.com>
2025-07-13 23:17:14 -0700

    u

--- entry-20250713-48L.sh
+++ entry-20250713-48L.sh
@@ -15,6 +15,6 @@ export OLMO_NUM_NODES_ENV_VAR=${BEAKER_REPLICA_COUNT}
 export LD_LIBRARY_PATH=/var/lib/tcpxo/lib64:$LD_LIBRARY_PATH
 # export NCCL_DEBUG=INFO 
 
-torchrun --rdzv_endpoint $BEAKER_LEADER_REPLICA_HOSTNAME:10086 --rdzv_id 20086 --rdzv_backend c10d --nnodes ${BEAKER_REPLICA_COUNT} --nproc-per-node ${BEAKER_ASSIGNED_GPU_COUNT} --node_rank "${BEAKER_REPLICA_RANK}" ./src/scripts/train/OLMoE3-dev-48l-jul10-gcp-warm1.py train OLMo3-moe-integrationtest-5-48L ai2/augusta-google-1 | tee /results/$$HOSTNAME.$BEAKER_REPLICA_RANK.log
+torchrun --rdzv_endpoint $BEAKER_LEADER_REPLICA_HOSTNAME:10086 --rdzv_id 20086 --rdzv_backend c10d --nnodes ${BEAKER_REPLICA_COUNT} --nproc-per-node ${BEAKER_ASSIGNED_GPU_COUNT} --node_rank "${BEAKER_REPLICA_RANK}" ./src/scripts/train/OLMoE3-dev-48l-jul10-gcp-warm1.py train OLMo3-moe-integrationtest-5-48L ai2/augusta-google-1
 
 
