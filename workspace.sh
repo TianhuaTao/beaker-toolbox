cf52b35817b6a3abd5ad07bf78b13e10b88d4aa7
Tianhua Tao <taotianhua@outlook.com>
2025-12-06 23:50:00 +0000

    t

--- workspace.sh
+++ workspace.sh
@@ -13,7 +13,7 @@ pkill -9 -f "train/OLMoE3-"
 export WANDB_API_KEY=61753d825c2bec08062290674ce9e3585bf31db3
 torchrun --nproc-per-node=8 OLMo-core/src/scripts/train/OLMoE3-nov25.py train OLMoE3-nov25 ai2/augusta
 
-nsys profile -t nvtx,cuda --capture-range=cudaProfilerApi --force-overwrite=true --output OLMoE3-nov25.nsys-rep torchrun --nproc-per-node=8 OLMo-core/src/scripts/train/OLMoE3-nov25.py train OLMoE3-nov25 ai2/augusta
+nsys profile -t nvtx,cuda --capture-range=cudaProfilerApi --capture-range-end=stop --force-overwrite=true --output OLMoE3-nov25.nsys-rep torchrun --nproc-per-node=8 OLMo-core/src/scripts/train/OLMoE3-nov25.py train OLMoE3-nov25 ai2/augusta
 
 
 scp -P30255 root@augusta-gcp-299:/workspace/prof_5_OLMoE3-nov25.nsys-rep ./
