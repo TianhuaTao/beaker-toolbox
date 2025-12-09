2c5510a0fa41b188fc31ac8e7fa707c1d3f566f7
Tianhua Tao <taotianhua@outlook.com>
2025-12-09 22:45:06 +0000

    tmp commit to update beaker-toolbox

--- workspace.sh
+++ workspace.sh
@@ -1,5 +1,5 @@
 # GCP multi node
-bash /workspace/beaker-toolbox/run_script_remotely.sh /workspace/hostfile /workspace/beaker-toolbox/node_cmd-ablation-train-general.sh OLMoE3-nov25
+bash /workspace/beaker-toolbox/run_script_remotely.sh /workspace/hostfile /workspace/beaker-toolbox/node_cmd-ablation-train-general.sh OLMoE3-dec9
 
 while read -r host; do
     ssh -p30255 -o "StrictHostKeyChecking no" "$host" "pkill -9 -f \"train/OLMoE3-\"" &
