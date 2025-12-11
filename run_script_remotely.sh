bb65f4d38f772358752c966c35071b20cab186e2
Tianhua Tao <taotianhua@outlook.com>
2025-12-11 20:03:14 +0000

    tmp commit to update beaker-toolbox

--- run_script_remotely.sh
+++ run_script_remotely.sh
@@ -77,7 +77,7 @@ while IFS= read -r HOST; do
   # Putting '&' at the end runs the ssh command in the background locally.
   # Use -n with SSH -n tells SSH to redirect its standard input from /dev/null, so it cannot eat your local script’s input
   ssh -n -p 30255 -o StrictHostKeyChecking=no "$HOST" "$REMOTEPATH $index '$HOSTFILE' '$TIMESTAMP' $EXTRA_ARGS " 2>&1 | sed "s/^/[$HOST - $index] /"    &
-  sleep 1
+  sleep 0.8
 #   echo "ssh return code: $?"
 #   echo "Submitted remote script on host '$HOST' with index=$index."
 # | tee "${HOST//[^a-zA-Z0-9_]/_}-${TIMESTAMP}.log"
