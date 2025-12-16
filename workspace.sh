86460c5d1e66862e3dca04f4ce7616d836c6e60a
Tianhua Tao <taotianhua@outlook.com>
2025-12-16 08:22:37 +0000

    t

--- workspace.sh
+++ workspace.sh
@@ -30,3 +30,11 @@ scp -P30255 root@augusta-gcp-299:/workspace/prof_5_OLMoE3-nov25.nsys-rep ./
 
 
 torchrun --rdzv_endpoint augusta-gcp-280:29500 --rdzv_id 33333 --rdzv_backend c10d --nnodes 64 --nproc-per-node 8 --node_rank 55 ./src/scripts/train/OLMoE3-dec10.py train OLMoE3-dec10 ai2/augusta
+
+
+sudo apt install bwm-ng
+
+# gcloud
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update && sudo apt-get install google-cloud-cli
